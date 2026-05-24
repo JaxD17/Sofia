a821c6099a1b6ed6f36310fab7ee2ffef87be7f2
JaxD17 <alexandr.jr17@gmail.com>
2026-05-24 02:27:19 -0500

    fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { viteSingleFile } from "vite-plugin-singlefile";
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: "/Sofia/",  // ✅ ESTA ES LA CLAVE
     plugins: [react(), tailwindcss(), viteSingleFile()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -21,10 +22,7 @@ export default defineConfig(({mode}) => {
       assetsInlineLimit: 100000000,
     },
     server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
-      // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
   };
